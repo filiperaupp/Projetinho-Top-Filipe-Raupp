a6233e42dd28052b2d1b428f7c77d4af95e06a64
Filipe <filipe.raupp@hotmail.com>
2018-02-28 13:31:30 -0300

    desafio 02

---
+++ Desafio 02/projetinho-top.js
@@ -0,0 +1,9 @@
+function shotAlert() {
+    var texto = document.getElementById("textoInput").value;
+    alert(`${texto}, Resultado de 2+2 = ${2+2}`);
+}
+
+function surprise() {
+    var texto = 'tomou dedadinha RSRSRS'
+    alert(`${texto}`);
+}
